245fd31519961388f4a5b7350199a766f794df78
Tukeshe <tkachave.123@gmail.comm>
2025-04-24 16:51:22 +0530

    Vite config file changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Prescription/',
+  base:"/",
   plugins: [
     [react()],
     tailwindcss(),
